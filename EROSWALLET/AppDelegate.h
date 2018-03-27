41fe8e9f3e74e295599b181cc96cb6a7331fdf4f
nestcode <nestcode@nestcodes.local>
2018-03-27 11:21:37 +0530

    Initial Commit

---
+++ EROSWALLET/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EROSWALLET
+//
+//  Created by nestcode on 3/27/18.
+//  Copyright © 2018 nestcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EROSWALLET/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EROSWALLET
+//
+//  Created by nestcode on 3/27/18.
+//  Copyright © 2018 nestcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
